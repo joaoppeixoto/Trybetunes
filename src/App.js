e86b5052b31241c635aeed4e76d0df52a0261cdd
Joao Peixoto <joao.peixoto678@gmail.com>
2022-11-03 17:16:53 -0300

    Requisito 1 ok

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
-//class
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import Search from './Search';
+import Login from './tests/Login';
+import Album from './Album';
+import Favorites from './Favorites';
+import Profile from './Profile';
+import ProfileEdit from './ProfileEdit';
+import NotFound from './NotFound';
+
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <div>
+        <p>TrybeTunes </p>
+        <BrowserRouter>
+          <Login />
+          <Search />
+          <Album />
+          <Favorites />
+          <Profile />
+          <ProfileEdit />
+          <NotFound />
+        </BrowserRouter>
+      </div>
+
+    );
   }
 }
 
