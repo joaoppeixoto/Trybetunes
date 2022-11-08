33bcec1ee61e49e2de5ea1694c158f4acef059aa
Joao Peixoto <joao.peixoto678@gmail.com>
2022-11-08 12:40:47 -0300

    Requisito 1 ok

--- src/App.js
+++ src/App.js
@@ -7,14 +7,42 @@ import Favorites from './Favorites';
 import Profile from './Profile';
 import ProfileEdit from './ProfileEdit';
 import NotFound from './NotFound';
+// import { bool } from 'prop-types';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      button: true,
+
+    };
+  }
+
+  handleInputCHange = (event) => {
+    const { name, type, button } = event.target;
+    const value = type === 'button' ? button : event.target.value;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  validateForm = () => {};
+
   render() {
+    const { name, button } = this.state;
     return (
       <div>
         <p>TrybeTunes </p>
         <BrowserRouter>
-          <Login />
+          <Login
+            handleInputCHange={ this.handleInputCHange }
+            state={ {
+              name, button,
+            } }
+            { ...this.state }
+            isFormValid={ this.validateForm() }
+          />
           <Search />
           <Album />
           <Favorites />
@@ -22,6 +50,7 @@ class App extends React.Component {
           <ProfileEdit />
           <NotFound />
         </BrowserRouter>
+
       </div>
 
     );
--- src/tests/Login.jsx
+++ src/tests/Login.jsx
@@ -2,8 +2,23 @@ import React, { Component } from 'react'
 
 class Login extends Component {
   render() {
+    const { handleInputCHange,
+         state: {name, button, }, isFormValid } = this.props;
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login" >Login
+      <label htmlFor='login'>
+        <input data-testid="login-name-input"
+        type='text'
+        name={name} 
+        value={name}
+        onChange={handleInputCHange}/>
+      </label>
+
+      <button data-testid="login-submit-button"
+      type='button' button={button} disabled={!isFormValid} onChange={handleInputCHange}>Entrar</button>
+      
+      
+      </div>
     )
   }
 }
