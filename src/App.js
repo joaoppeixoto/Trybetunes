efc1aa3179d50db00f278dbdb7dab2c83f859f98
Joao Peixoto <joao.peixoto678@gmail.com>
2022-11-13 14:58:23 -0300

    requisito 4

--- src/App.js
+++ src/App.js
@@ -1,56 +1,62 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import Login from './Login';
 import Search from './Search';
-import Login from './tests/Login';
 import Album from './Album';
 import Favorites from './Favorites';
 import Profile from './Profile';
 import ProfileEdit from './ProfileEdit';
 import NotFound from './NotFound';
-// import { bool } from 'prop-types';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      name: '',
-      button: true,
-
-    };
-  }
-
-  handleInputCHange = (event) => {
-    const { name, type, button } = event.target;
-    const value = type === 'button' ? button : event.target.value;
-    this.setState({
-      [name]: value,
-    });
-  };
-
-  validateForm = () => {};
-
   render() {
-    const { name, button } = this.state;
     return (
       <div>
         <p>TrybeTunes </p>
-        <BrowserRouter>
-          <Login
-            handleInputCHange={ this.handleInputCHange }
-            state={ {
-              name, button,
-            } }
-            { ...this.state }
-            isFormValid={ this.validateForm() }
+        <Switch>
+          <Route
+            exact
+            path="/"
+            component={ Login }
           />
-          <Search />
-          <Album />
-          <Favorites />
-          <Profile />
-          <ProfileEdit />
-          <NotFound />
-        </BrowserRouter>
 
+          <Route
+            exact
+            path="/search"
+            component={ Search }
+          />
+
+          <Route
+            exact
+            path="/album/:id"
+            component={ Album }
+          />
+
+          <Route
+            exact
+            path="/favorites"
+            component={ Favorites }
+          />
+
+          <Route
+            exact
+            path="/profile"
+            component={ Profile }
+          />
+
+          <Route
+            exact
+            path="/profile/edit"
+            component={ ProfileEdit }
+          />
+
+          <Route
+            exact
+            path="/notfound"
+            component={ NotFound }
+          />
+          <NotFound />
+        </Switch>
       </div>
 
     );
---
+++ src/Login.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { createUser } from './services/userAPI';
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      login: '',
+      isDisabled: true,
+    };
+  }
+
+  handleChange = (target) => {
+    this.setState({ [target.name]: target.value,
+    }, () => this.validateButton());
+  };
+
+  validateButton = () => {
+    const { login } = this.state;
+    const number = 3;
+    const invalid = login.length < number;
+    this.setState({
+      isDisabled: invalid,
+    });
+  };
+
+  //   redirectSearch = (event) => {
+  //     event.preventDefault();
+  //     const { history } = this.props;
+  //     history.push('/search');
+  //   }
+
+  render() {
+    const { login, isDisabled } = this.state;
+    console.log(isDisabled);
+    return (
+
+      <div data-testid="page-login">
+        Login
+        <form onSubmit={ createUser }>
+          <label htmlFor="login">
+            <input
+              data-testid="login-name-input"
+              placeholder="Digite seu login"
+              id="login"
+              type="text"
+              name="login"
+              value={ login }
+              onChange={ ({ target }) => this.handleChange(target) }
+            />
+          </label>
+
+          <button
+            data-testid="login-submit-button"
+            type="submit"
+            name="button"
+            disabled={ isDisabled }
+          >
+            {' '}
+            Entrar
+          </button>
+
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
